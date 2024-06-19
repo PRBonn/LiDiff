95effb2f20517219c39c512e58000d6145a0f607
Ellington Kirby <ellingtonkirby31@gmail.com>
2024-06-19 13:06:58 +0000

    make multi gpu training match single gpu

--- lidiff/train_objects_full_diff.py
+++ lidiff/train_objects_full_diff.py
@@ -85,6 +85,8 @@ def main(config, weights, checkpoint, test):
                         max_epochs= cfg['train']['max_epoch'],
                         callbacks=[lr_monitor, checkpoint_saver],
                         check_val_every_n_epoch=10,
+                        num_sanity_val_steps=1,
+                        limit_val_batches=1,
                         accelerator='gpu',
                         strategy="ddp",
                         )
